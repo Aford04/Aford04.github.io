851aaf319a5122b0d6c0fd8843a5d8866a582174
Aford0 <aford5@my.waketech.edu>
2025-07-16 18:59:51 -0400

    gradebook

---
+++ gradebook.js
@@ -0,0 +1,13 @@
+// Stub: Fetch grade data (to be connected to PostgreSQL in future)
+function fetchGradeData() {
+    console.log("fetchGradeData() called");
+}
+
+// Stub: Populate the gradebook table
+function populateGradebook() {
+    console.log("populateGradebook() called");
+}
+
+// Trigger both stub functions on page load
+fetchGradeData();
+populateGradebook();
